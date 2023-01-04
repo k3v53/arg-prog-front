0df019e0e46acaee76f3c6919295a1a221c2f6a6
k3v <25158905+k3v53@users.noreply.github.com>
2023-01-04 20:13:16 -0300

    Init Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'arg_prog_front_kevin_vicente';
+}
