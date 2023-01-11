23140c6e652568a04f3b4ba89969b1e81d7b9fe4
k3v <25158905+k3v53@users.noreply.github.com>
2023-01-11 00:36:05 -0300

    Basic rearrangement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -7,7 +8,7 @@ import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, LandingComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
-
+  faAngleDown = faAngleDown;
+  faReact = faReact;
 }
