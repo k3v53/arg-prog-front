15b2733ad1bb3fab0482385ad755971835cbda91
k3v <25158905+k3v53@users.noreply.github.com>
2023-01-26 13:44:31 -0300

    Updated languages entries

--- src/app/components/skills/language-entry/language-entry.component.ts
+++ src/app/components/skills/language-entry/language-entry.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-language-entry',
   templateUrl: './language-entry.component.html',
-  styleUrls: ['./language-entry.component.css']
+  styleUrls: ['./language-entry.component.css'],
 })
 export class LanguageEntryComponent {
-
+  @Input() name: string = 'Language';
+  @Input() expertise: string = 'Native';
 }
